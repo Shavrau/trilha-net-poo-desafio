a0762840417dd8cde7de0aebbdd0f0fc8b40ad3e
Rhian Mendes Souza <rhianmendes301@gmail.com>
2023-11-07 10:33:48 -0300

    Resolução do exercicio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia= new Nokia("99219112","nokia 1","imei 1",8);
+Smartphone iphone = new Iphone("99212452", "iphone12", "imei 2", 16);
+
+iphone.InstalarAplicativo("facebook");
+nokia.InstalarAplicativo("twitch");
+
